e553b31a6ae83c35ae9eb0d30b4c22bdb256ce82
Kyeonghoon <zhaqj12@naver.com>
2022-12-16 10:21:47 +0900

    회원 웹 기능 - 회원등록, html에서 MemberForm에 접근해서 setter를 넣어줄수 있다?..

--- src/main/java/study/studyspring/controller/MemberController.java
+++ src/main/java/study/studyspring/controller/MemberController.java
@@ -2,6 +2,9 @@ package study.studyspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import study.studyspring.domain.Member;
 import study.studyspring.service.MemberService;
 
 @Controller
@@ -12,4 +15,20 @@ public class MemberController {
     public MemberController(MemberService memberSerivce) {
         this.memberSerivce = memberSerivce;
     }
+
+    @GetMapping("/members/new")
+    public String create(MemberForm form){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String createForm(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberSerivce.join(member);
+
+        return "redirect:/";
+    }
+
 }
